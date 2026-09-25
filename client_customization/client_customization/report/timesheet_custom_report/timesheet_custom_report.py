a3b3385c1342a1a26ffc326ec568cdd9649deefa
pooja <pooja.c@psdigitise.com>
2026-09-25 13:39:18 +0530

    Fix KeyError by using Script Report and defaulting missing filters

--- client_customization/client_customization/report/timesheet_custom_report/timesheet_custom_report.py
+++ client_customization/client_customization/report/timesheet_custom_report/timesheet_custom_report.py
@@ -5,72 +5,67 @@ def execute(filters=None):
     if not filters:
         filters = {}
 
-    conditions = []
-    
-    if filters.get("employee"):
-        conditions.append("ts.employee = %(employee)s")
-    if filters.get("from_time"):
-        conditions.append("tl.from_time >= %(from_time)s")
-    if filters.get("to_time"):
-        conditions.append("tl.to_time <= %(to_time)s")
-        
-    where_clause = " AND ".join(conditions)
-    if where_clause:
-        where_clause = " AND " + where_clause
+    # Ensure all filter keys exist to avoid KeyError when frappe.db.sql formats the query
+    filters.setdefault("employee", "")
+    filters.setdefault("from_time", "")
+    filters.setdefault("to_time", "")
 
-    query = f"""
-        SELECT
-            ts.name AS "timesheet",
-            ts.employee AS "employee",
-            ts.employee_name AS "employee_name",
-            tl.task AS "task",
-            p.project_name AS "project",
-            t.subject AS "task_subject",
-            t.description AS "task_description",
-            t.status AS "task_status",
-            tl.activity_type AS "activity_type",
-            tl.from_time AS "from_time",
-            tl.to_time AS "to_time",
-            tl.hours AS "hours",
-            t.custom_assigned_by AS "assigned_by",
-            t.custom_assigned_start_date_ AS "assigned_start_date"
-        FROM
-            `tabTimesheet` ts
-        INNER JOIN
-            `tabTimesheet Detail` tl
-            ON tl.parent = ts.name
-        LEFT JOIN
-            `tabTask` t
-            ON t.name = tl.task
-        LEFT JOIN
-            `tabProject` p
-            ON p.name = t.project
-        WHERE
-            ts.docstatus < 2
-            {where_clause}
-        ORDER BY
-            ts.start_date DESC,
-            ts.name DESC
+    query = """
+    SELECT
+        ts.name AS "timesheet",
+        ts.employee AS "employee",
+        ts.employee_name AS "employee_name",
+        tl.task AS "task",
+        p.project_name As "project",
+        t.subject AS "task_subject",
+        t.description AS "task_description",
+        t.status AS "task_status",
+        tl.activity_type AS "activity_type",
+        tl.from_time AS "from_time",
+        tl.to_time AS "to_time",
+        tl.hours AS "hours",
+        t.custom_assigned_by AS "assigned_by",
+        t.custom_assigned_start_date_ AS "assigned_start_date"
+    FROM
+        `tabTimesheet` ts
+    INNER JOIN
+        `tabTimesheet Detail` tl
+        ON tl.parent = ts.name
+    LEFT JOIN
+        `tabTask` t
+        ON t.name = tl.task
+    LEFT JOIN
+        `tabProject` p
+        ON p.name = t.project
+    WHERE
+        ts.docstatus < 2
+        AND (%(employee)s = '' OR ts.employee = %(employee)s)
+        AND (%(from_time)s = '' OR tl.from_time >= %(from_time)s)
+        AND (%(to_time)s = '' OR tl.to_time <= %(to_time)s)
+    ORDER BY
+        ts.start_date DESC,
+        ts.name DESC;
     """
+
+    data = frappe.db.sql(query, filters, as_dict=1)
     
     columns = [
-        {"fieldname": "timesheet", "label": "Timesheet", "fieldtype": "Link", "options": "Timesheet", "width": 150},
-        {"fieldname": "employee", "label": "Employee", "fieldtype": "Link", "options": "Employee", "width": 150},
-        {"fieldname": "employee_name", "label": "Employee Name", "fieldtype": "Data", "width": 180},
-        {"fieldname": "task", "label": "Task", "fieldtype": "Link", "options": "Task", "width": 150},
-        {"fieldname": "project", "label": "Project", "fieldtype": "Data", "width": 150},
-        {"fieldname": "task_subject", "label": "Task Subject", "fieldtype": "Data", "width": 200},
-        {"fieldname": "task_description", "label": "Task Description", "fieldtype": "Text", "width": 250},
-        {"fieldname": "task_status", "label": "Task Status", "fieldtype": "Data", "width": 120},
-        {"fieldname": "activity_type", "label": "Activity Type", "fieldtype": "Data", "width": 150},
-        {"fieldname": "from_time", "label": "From Time", "fieldtype": "Datetime", "width": 160},
-        {"fieldname": "to_time", "label": "To Time", "fieldtype": "Datetime", "width": 160},
-        {"fieldname": "hours", "label": "Hours", "fieldtype": "Float", "width": 100},
-        {"fieldname": "assigned_by", "label": "Assigned By", "fieldtype": "Data", "width": 150},
-        {"fieldname": "assigned_start_date", "label": "Assigned Start Date", "fieldtype": "Datetime", "width": 180}
+        "Timesheet:Link/Timesheet:150",
+        "Employee:Link/Employee:150",
+        "Employee Name:Data:180",
+        "Task:Link/Task:150",
+        "Project:Data:150",
+        "Task Subject:Data:200",
+        "Task Description:Text:250",
+        "Task Status:Data:120",
+        "Activity Type:Data:150",
+        "From Time:Datetime:160",
+        "To Time:Datetime:160",
+        "Hours:Float:100",
+        "Assigned By:Data:150",
+        "Assigned Start Date:Datetime:180"
     ]
-    
-    data = frappe.db.sql(query, filters, as_dict=1)
+
     return columns, data
 
 def get_xlsx_styles(metadata: XLSXMetadata) -> dict:
@@ -80,8 +75,8 @@ def get_xlsx_styles(metadata: XLSXMetadata) -> dict:
         "num_format": "dd-mm-yyyy hh:mm:ss",
     })
 
-    from_time_col = builder.field_index_map.get("from_time")
-    to_time_col = builder.field_index_map.get("to_time")
+    from_time_col = builder.field_index_map.get("From Time")
+    to_time_col = builder.field_index_map.get("To Time")
 
     if from_time_col is not None:
         builder.style_column(from_time_col, datetime_style)
